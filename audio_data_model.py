bec11fc80fa3e23c8ec687d234cf7a4bedee47f9
DustInAWhile <158524078+DustInAWhile@users.noreply.github.com>
2024-11-29 13:37:13 -0500

    Update audio_data_model.py
    Made a function which converts the audio file to wav, the first iteration anyway

--- audio_data_model.py
+++ audio_data_model.py
@@ -2,6 +2,7 @@ import librosa
 import numpy as np
 import soundfile as sf
 import scipy.signal as signal
+from pydub import AudioSegment
 
 class AudioDataModel: #Class for the audio data model.
     def __init__(self):
@@ -26,3 +27,19 @@ class AudioDataModel: #Class for the audio data model.
     
     def get_RTs(self): #obtains the values of the RT60.
         return self.RT60_values
+
+    def convert_to_wav(self, output_file=None):
+        if not self.audio_file:
+            raise ValueError("No audio file loaded. Please load an audio file first.")
+
+    # If no output path is provided, create one by changing the file extension to '.wav'
+        if output_file is None:
+            #(this assumes only one period in the filename)
+            output_file = self.audio_file.rsplit('.', 1)[0] + '.wav'
+
+        try:
+            audio = AudioSegment.from_file(self.audio_file)  # Load the audio using pydub
+            audio.export(output_file, format="wav")
+            print(f"File successfully converted to {output_file}")
+        except Exception as e:
+            print(f"Error during conversion: {e}")
