4debe7c3b8ecd8ff26403339eea89d54d7bfa985
Ian <iroth6113@floridapoly.edu>
2024-12-01 20:28:42 -0500

    Added code to main.py to allow for the program to be tested (created model object)

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import pydub
 
-def convert_mp3_to_wav(file_path):
+def convert_mp3_to_wav(file_path): #converts to a .wav file
     audio = pydub.AudioSegment.from_mp3(file_path)
     wav_file_path = file_path.replace(".mp3", ".wav")
     audio.export(wav_file_path, format="wav")
